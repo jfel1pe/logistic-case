2a8da625be06fbdaa3fa6fb834dddeb3035e8376
Felipe <jfelipejaramillo2000@gmail.com>
2026-04-30 17:28:31 -0500

    Correccion de tests, todo funcionando hasta aqui

--- src/test/java/com/logistic/logistic/service/LandShipmentServiceImplTest.java
+++ src/test/java/com/logistic/logistic/service/LandShipmentServiceImplTest.java
@@ -13,6 +13,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -25,6 +27,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 class LandShipmentServiceImplTest {
 
     @Mock
